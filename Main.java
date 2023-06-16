ce33f6f7ed4b5ef14feda6451d9bb6e0b68665e9
Poltorak Serguei <ps@z-wave.me>
2023-06-16 05:57:52 +0300

    Main updated

--- Main.java
+++ Main.java
@@ -4,19 +4,38 @@ import java.lang.reflect.Type;
 
 class Main {
     public static void main(String[] args) throws InterruptedException {
-
         ZWay zway = init();
 
         discover(zway);
 
         Thread.sleep(1000);
-        //zway.controller.setDefault();
-        //zway.controller.addNodeToNetwork(true);
-        //zway.controller.removeNodeFromNetwork(true);
+        
+        /*
+        zway.controller.setDefault();
+        Thread.sleep(10000);
+        zway.controller.removeNodeFromNetwork(true);
+        Thread.sleep(5000);
+        zway.controller.addNodeToNetwork(true);
+        */
 
-        //((ZWay.Device.Instance.SwitchBinary) zway.devices.get(2).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").bind(myCallback);
+        int nodeId = 2;
+        
         try {
-            System.out.println("switch level: " + String.valueOf(((ZWay.Device.Instance.SwitchBinary) zway.devices.get(2).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").getBool()));
+            ((ZWay.Device.Instance.SwitchBinary) zway.devices.get(nodeId).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").bind(new Demo());
+        } catch (ZWay.Data.NotFound e) {
+            System.out.println("Data is not found ");
+        } catch (ZWay.Data.NotAlive e) {
+            System.out.println("Data is not alive " + e.data.path);
+        } catch (java.lang.Exception e) {
+            System.out.println(e);
+        }
+        
+        try {
+            System.out.println("switch level: " + String.valueOf(((ZWay.Device.Instance.SwitchBinary) zway.devices.get(nodeId).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").getBool()));
+        } catch (ZWay.Data.NotFound e) {
+            System.out.println("Data is not found ");
+        } catch (ZWay.Data.NotAlive e) {
+            System.out.println("Data is not alive " + e.data.path);
         } catch (java.lang.Exception e) {
             System.out.println(e);
         }
@@ -26,7 +45,7 @@ class Main {
             Thread.sleep(1000);
             try {
                 System.out.println("switch");
-                ((ZWay.Device.Instance.SwitchBinary) zway.devices.get(2).instances.get(0).commandClassesByName.get("switchBinary")).set(s, 0);
+                ((ZWay.Device.Instance.SwitchBinary) zway.devices.get(nodeId).instances.get(0).commandClassesByName.get("switchBinary")).set(s, 0);
             } catch (java.lang.Exception e) {
                 System.out.println(e);
             }
@@ -34,11 +53,21 @@ class Main {
         }
     }
 
+    static class Demo implements ZWay.DataCallback {
+        public void dataCallback(ZWay.Data data, Integer type) {
+            try {
+                System.out.println(data.path + " = " + data.getBool() + " (type = " + type + ")"); 
+            } catch (ZWay.Data.NotAlive e) {
+                System.out.println("Data no alive " + e.data.path + " " + e);
+            }
+        }
+    }
+    
     public static ZWay init() {
         ZWay zway;
         try {
             System.out.println("init zway");
-            zway = new ZWay("zway", "/dev/ttyACM2", 115200, "z-way-root/config", "z-way-root/translations", "z-way-root/ZDDX", 0);
+            zway = new ZWay("zway", "/dev/ttyACM0", 115200, "z-way-root/config", "z-way-root/translations", "z-way-root/ZDDX", 0);
         } catch (java.lang.Exception e) {
             System.out.println(e);
             throw new RuntimeException();
