90151b9a6ac02ed58a47bcd7732ff937b5ac051c
Poltorak Serguei <ps@z-wave.me>
2023-06-19 15:49:18 +0300

    Rework of Main

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 import ZWayJNIWrapper.*;
 
 class Main {
+    private static ZWay zway;
+    
     public static void main(String[] args) throws Exception {
-        ZWay zway = init();
+        zway = init();
 
         zway.bind(new DeviceDemo());
 
         discover(zway);
 
-        Thread.sleep(1000);
-        
         /*
         zway.controller.setDefault();
         Thread.sleep(10000);
@@ -20,26 +20,6 @@ class Main {
 
         int nodeId = 2;
         
-        try {
-            ((ZWay.Device.Instance.SwitchBinary) zway.devices.get(nodeId).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").bind(new Demo());
-        } catch (ZWay.Data.NotFound e) {
-            System.out.println("Data is not found ");
-        } catch (ZWay.Data.NotAlive e) {
-            System.out.println("Data is not alive " + e.data.path);
-        } catch (java.lang.Exception e) {
-            System.out.println(e);
-        }
-        
-        try {
-            System.out.println("switch level: " + String.valueOf(((ZWay.Device.Instance.SwitchBinary) zway.devices.get(nodeId).instances.get(0).commandClassesByName.get("switchBinary")).data.get("level").getBool()));
-        } catch (ZWay.Data.NotFound e) {
-            System.out.println("Data is not found ");
-        } catch (ZWay.Data.NotAlive e) {
-            System.out.println("Data is not alive " + e.data.path);
-        } catch (java.lang.Exception e) {
-            System.out.println(e);
-        }
-
         boolean s = false;
         while (zway.isRunning()) {
             Thread.sleep(1000);
@@ -55,6 +35,7 @@ class Main {
 
     static class Demo implements ZWay.DataCallback {
         public void dataCallback(ZWay.Data data, Integer type) {
+            if (type == ZWay.Data.deleted) return;
             try {
                 System.out.println(data.path + " = " + data.getBool() + " (type = " + type + ")"); 
             } catch (ZWay.Data.NotAlive e) {
@@ -65,7 +46,32 @@ class Main {
 
     static class DeviceDemo implements ZWay.DeviceCallback {
         public void deviceCallback(Integer type, Integer deviceId, Integer instanceId, Integer commandClassId) {
-            System.out.println("Device Callback of type " + type + " was caught. The arguments are: device ID: " + deviceId + "; instance ID: " + instanceId + "; command class ID: " + commandClassId + ";");
+            if (type == ZWay.deviceAdded) {
+                System.out.println("Device added " + deviceId);
+            } else if (type == ZWay.deviceRemoved) {
+                System.out.println("Device removed " + deviceId);
+            } else if (type == ZWay.instanceAdded) {
+                System.out.println("Instance added " + deviceId + ":" + instanceId);
+            } else if (type == ZWay.instanceRemoved) {
+                System.out.println("Instance removed " + deviceId + ":" + instanceId);
+            } else if (type == ZWay.commandAdded) {
+                System.out.println("Command Class added " + deviceId + ":" + instanceId + " " + commandClassId);
+            } else if (type == ZWay.commandRemoved) {
+                System.out.println("Command Class removed " + deviceId + ":" + instanceId + " " + commandClassId);
+            }
+
+            // subscribe to switchBinary.data.level
+            if (type == zway.commandAdded && commandClassId == ZWay.commandClassIdByName.get("switchBinary")) {
+                try {
+                    ((ZWay.Device.Instance.SwitchBinary) zway.devices.get(deviceId).instances.get(instanceId).commandClassesByName.get("switchBinary")).data.get("level").bind(new Demo());
+                } catch (ZWay.Data.NotFound e) {
+                    System.out.println("Data is not found ");
+                } catch (ZWay.Data.NotAlive e) {
+                    System.out.println("Data is not alive " + e.data.path);
+                } catch (java.lang.Exception e) {
+                    System.out.println(e);
+                }
+            }
         }
     }
     
